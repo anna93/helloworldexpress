626a545dcbec42b12180b5a107dcd9a09d7f2852
Vin_code <vinit.payal@mebelkart.com>
2016-03-13 15:53:45 +0530

    Add templating using ejs-locals node module

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var express = require('express');
-var app = express();
+var engine = require('ejs-locals');
+var app=express();
 var path    = require("path");
+
+
+app.engine('ejs',engine);
+
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-    res.render("index.ejs");
+    res.json({msg: 'Our HomePage'});
 });
 
 app.get('/home', function (req, res) {
-    res.render('home.ejs',{
+    res.render('home',{
         title: 'Home Page',
         pageDescription: 'Simple Page for displaying'
     });
