e9af41d523e573771be14f4efaa93b2c7c5bc887
Vin_code <vinit.payal@mebelkart.com>
2016-03-13 14:09:09 +0530

    Setup views folder and ejs module

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
+app.get('/home', function (req, res) {
+    res.render('home.ejs',{
+        title: 'Home Page',
+        pageDescription: 'Simple Page for displaying'
+    });
+});
+
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
