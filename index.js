a6c526c91d5fd998e88c58f65ea8284398ae117d
Vin_code <vinit.payal@mebelkart.com>
2016-03-16 01:16:26 +0530

    Login using redis

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var engine = require('ejs-locals');
 var app=express();
 var path    = require("path");
-
+var redis = require('redis');
+var client = redis.createClient();
 
 app.engine('ejs',engine);
 
@@ -12,6 +13,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.json({msg: 'Our HomePage'});
+});
+
+app.get('/validateLogin', function (req,res) {
+    var emailIdEnteredByUSer=req.query.emailId;
+    var passwordEnteredByUser=req.query.password;
+    client.sismember("userList",emailIdEnteredByUSer,function(err,reply){
+        if(reply==0) {
+            return res.json({'loginStatus': 0,'msg':' Email id isn\'t registered with us'});
+        }
+        else{
+            client.get(emailIdEnteredByUSer+":password", function (err,reply) {
+               if(reply==passwordEnteredByUser){
+                   return res.json({'loginStatus': 1});
+               }
+                else{
+                   return res.json({'loginStatus': 0,'msg':' Wrong password entered please check your password'});
+               }
+            });
+
+        }
+    });
 });
 
 app.get('/home', function (req, res) {
